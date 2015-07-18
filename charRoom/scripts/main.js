771a3a342fcbbc3d0f7cbbc39ba3fa21b9fd5dce
wyxjsdf <wuyuexin333@gmail.com>
2015-07-18 18:09:05 +0800

    no message

--- charRoom/scripts/main.js
+++ charRoom/scripts/main.js
@@ -1,17 +1,19 @@
-﻿var nickName = '', arrayName =[];
+﻿var nickName = '';						//当前使用的昵称名
+var arrayName =[];						//在线用户
+var AllMessage = 0;						//接受到信息条数
 var myDataRef = new Firebase('https://burning-heat-1712.firebaseio.com/');
-window.setInterval(showOnline, 5000);
+window.setInterval(showOnline, 5000);	//每隔5s向服务器发送信息
 function showOnline()
 {
 	var myDate = new Date();
 	myDataRef.child(nickName).set({name:nickName, time:myDate.getTime()});
 }
-myDataRef.on('child_added', function(snapshot) {
+myDataRef.on('child_added', function(snapshot) {		//firebase新增节点响应事件
 	var message = snapshot.val();
 	var myDate = new Date();
-	if (!('time' in message))
+	if (!('time' in message))							//判断信息类型
 		displayChatMessage(message.name, message.text);
-	else if (myDate.getTime() - message.time <= 60000)
+	else if (myDate.getTime() - message.time <= 60000)	//判断用户是否在线
 	{
 		if ($.inArray(message.name, arrayName) == -1){
 			arrayName.push(message.name);
@@ -19,7 +21,7 @@ myDataRef.on('child_added', function(snapshot) {
 		}
 	}
 });
-myDataRef.on('child_removed', function(oldChildsnapshot) {
+myDataRef.on('child_removed', function(oldChildsnapshot) {		//用户下线通知
 	var message = oldChildsnapshot.val();
 	var p = $.inArray(message.name, arrayName);
 	if (p != -1)
@@ -29,27 +31,37 @@ myDataRef.on('child_removed', function(oldChildsnapshot) {
 	}
 });
 
-$(window).unload(function(){
+$(window).keydown(function(e){									//键盘响应事件
+	if (e.ctrlKey && e.keyCode == 13)
+		sendMessage();
+});
+$(window).unload(function(){									//离开页面时用户下线
 	if (nickName != '')
 	{
 		removeName(nickName);
 	}
 });
-function removeName(nickName){
+function removeName(nickName){									//删去用户信息
 	myDataRef.child(nickName).remove();
 }
 
-function displayChatMessage(name, text) {
-	for (var i = 0; i < text.length; i++)
+function displayChatMessage(name, text) {						//将信息加入到页面中去
+	for (var i = 0; i < text.length; i++)						//处理换行符
 		if (text.charAt(i) == '\n'){
 			text = text.substring(0, i) + '<br/>' + text.substring(i + 1);
 			i += 4;
 		}
+	AllMessage++;
+	if (AllMessage > 200)										//控制页面显示的信息条数
+	{
+		AllMessage--;
+		$('#messageBox').children()[0].remove();
+	}
 	$('#messageBox')[0].innerHTML += '<div class ="oneMessage"><div class = "oneName"> '+ name + ':</div><div class = "oneContent"><div class = "oneContentText">' + text + '</div> </div></div>';
 //	$('<div class = "oneMessage"/>').append($('<div class="oneName"/>').text(name+': ')).append($('<div class="oneContent"/>').append($('<div class="oneContentText"/>').text(text))).appendTo($('#messageBox'));
 	$('#messageBox')[0].scrollTop = $('#messageBox')[0].scrollHeight;
 };
-function sendMessage(){
+function sendMessage(){											//发送信息
 	var text = $('#inputMessage').val();
 	if (nickName == '') {
 		changeNickName();
@@ -59,9 +71,9 @@ function sendMessage(){
 	$('#inputMessage').val('');
 	}
 }
-function changeNickName()
+function changeNickName()										//修改昵称
 {
-	apprise("请输入你的昵称",{input:true},function(aText){
+	apprise("请输入你的昵称",{input:true},function(aText){		//新增对话框
 		if ($.inArray(aText, arrayName) != -1)
 			return 1;
 		else{
